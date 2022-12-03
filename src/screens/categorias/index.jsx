d1c9a98f16fb27ea1ea90ee7b9e85f56a2d5e1b0
Alan Uzal <uzal.alan@gmail.com>
2022-12-03 15:42:55 -0300

    Agregando prettier

---
+++ src/screens/categorias/index.jsx
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native";
+
+import React from "react";
+import {styles} from "./styles";
+
+const Categorias = ()=>{
+    return(
+        <View style={styles.container}>
+            <Text>Categorias</Text>
+        </View>
+    )
+}
+
+export default Categorias;
